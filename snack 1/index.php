67b8d29e75de57366ac1740bf75436678a4a23ad
Andrea Contestabile <andreacontest@gmail.com>
2020-10-05 16:36:49 +0200

    Snack 1 - Partite di Basket

---
+++ snack 1/index.php
@@ -0,0 +1,79 @@
+<!-- SNACK 1
+Creiamo un array 'matches' contenente altri array i quali rappresentano delle partite di basket di un’ipotetica tappa del calendario.
+Ogni array della partita avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite.
+Stampiamo a schermo tutte le partite con questo schema:
+Olimpia Milano - Cantù | 55-60 -->
+
+<?php
+
+$matches = [
+  [
+    "squadra di casa" => "Virtus Bologna",
+    "squadra ospite" => "Cremona",
+    "punti squadra di casa" => 48,
+    "punti squadra ospite" => 58
+  ],
+  [
+    "squadra di casa" => "Fortitudo Bologna",
+    "squadra ospite" => "Trento",
+    "punti squadra di casa" => 44,
+    "punti squadra ospite" => 53
+  ],
+  [
+    "squadra di casa" => "Roma",
+    "squadra ospite" => "Sassari",
+    "punti squadra di casa" => 54,
+    "punti squadra ospite" => 49
+  ],
+  [
+    "squadra di casa" => "Trieste",
+    "squadra ospite" => "Olimpia Milano",
+    "punti squadra di casa" => 55,
+    "punti squadra ospite" => 60
+  ],
+  [
+    "squadra di casa" => "Venezia",
+    "squadra ospite" => "Pesaro",
+    "punti squadra di casa" => 44,
+    "punti squadra ospite" => 58
+  ],
+  [
+    "squadra di casa" => "Reggiana",
+    "squadra ospite" => "Brindisi",
+    "punti squadra di casa" => 57,
+    "punti squadra ospite" => 52
+  ],
+  [
+    "squadra di casa" => "Treviso",
+    "squadra ospite" => "Brescia",
+    "punti squadra di casa" => 41,
+    "punti squadra ospite" => 47
+  ],
+  [
+    "squadra di casa" => "Varese",
+    "squadra ospite" => "Cantù",
+    "punti squadra di casa" => 61,
+    "punti squadra ospite" => 59
+  ]
+];
+
+ ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Snack 1</title>
+    <h1>Calendario Partite Lega A Basket</h1>
+    <ul class="matches-list">
+      <?php
+      for ($i = 0; $i < count($matches); $i++) {
+        $match = $matches[$i];
+        echo "<li>" . $match["squadra di casa"] . " - " . $match["squadra ospite"] . " | " . $match["punti squadra di casa"] . " - " . $match["punti squadra ospite"] . "</li>";
+      }
+      ?>
+    </ul>
+  </head>
+  <body>
+
+  </body>
+</html>
